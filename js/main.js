c9a5d5ffc4a1f920f1b115985a3818019796480b
AsadbekMakhmudjonov <asadbek.mah.@gmail.com>
2021-09-26 19:41:10 +0500

    finished paginatino

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-// BOOKS
-
-// localStorage.removeItem("bookmarks");
+// GLOBAL VARS
 let watchList =  JSON.parse(localStorage.getItem("bookmarks")) || [];
 let languages = [];
 let countries = [];
+let foundBooks = books;
+let titleRegex;
+let countryRegex;
 
-console.log(`Watch List:  ${watchList}`);
+// PAGINATION
+let TOTAL_RESULTS = 10; //ALL OBJS TO SHOW
+let PER_PAGE_COUNT = 10; // OBJS COUNT TO SHOW FOR PER PAGE
+let CURRENT_PAGE = 1; // ACTIVE PAGE
+let PAGES_LINK_TO_SHOW = 5; // ONLY ODD NUMBERS
+let NEIGHBOUR_PAGES_COUNT = Math.floor(PAGES_LINK_TO_SHOW / 2);
+let PAGES_COUNT = Math.ceil(TOTAL_RESULTS / PER_PAGE_COUNT); // TOTAL PAGES
+
+
+
 // DOM
 // BOOKS LIST WRAPPER
 const elBooks = document.querySelector(".books");
@@ -19,13 +29,21 @@ const elSearchLanguagesSelect = elMainForm.querySelector(".js-filter-language");
 const elSearchYear = elMainForm.querySelector(".js-search-year");
 const elSearchFilter = elMainForm.querySelector(".js-filter-specail");
 
-// GLOBAL VARS
-let titleRegex;
-let countryRegex;
+// PAGINATION DOM
+const elPagination = document.querySelector(".js-pagination");
+const elPaginationStartLink = document.querySelector(".js-pagination-start");
+const elPaginationPrevLink = document.querySelector(".js-pagination-prev");
+const elPaginationList = document.querySelector(".js-pagination-list");
+const elPaginationNextLink = document.querySelector(".js-pagination-next");
+const elPaginationEndLink = document.querySelector(".js-pagination-end");
+const elPagnationItemElementTemplate = document.querySelector("#pagination-item-template").content;
+
+
 // FUNCTIONS CODE
 
 // CREATING AND ALL FILMS
 function showBooksElement(givenBooks, titleRegex = "") {
+
   elBooks.innerHTML = "";
   const elBooksFragment = document.createDocumentFragment();
   for (const book of givenBooks) {
@@ -151,10 +169,21 @@ function onBookSearchFormSubmit(evt) {
   // countryRegex = new RegExp(elSearchCountry.value.trim(), "gi");
 
 
-  let foundBooks = findBook();
+  foundBooks = findBook();
   if(foundBooks.length > 0){
+
     sortBooks(foundBooks , elSearchFilter);
-    showBooksElement(foundBooks, titleRegex);
+
+
+    TOTAL_RESULTS = foundBooks.length;
+    PAGES_COUNT = Math.ceil(TOTAL_RESULTS / PER_PAGE_COUNT);
+    CURRENT_PAGE = 1;
+
+    let booksToShow = foundBooks.slice(0, PER_PAGE_COUNT);
+
+
+    showBooksElement(booksToShow, titleRegex);
+    showPagenation();
   }
 
 }
@@ -238,6 +267,97 @@ function removeFromWatchlist(bookId) {
   return watchList.splice(index, 1);
 }
 
+function showPagenation() {
+
+
+  let startIndex = (CURRENT_PAGE - 1) * PER_PAGE_COUNT;
+  let endIndex = startIndex + PER_PAGE_COUNT;
+  showBooksElement(foundBooks.slice(startIndex, endIndex),titleRegex)
+
+
+
+  elPaginationList.innerHTML = "";
+  let startPage = CURRENT_PAGE - NEIGHBOUR_PAGES_COUNT;
+  let endPage = CURRENT_PAGE + NEIGHBOUR_PAGES_COUNT;
+
+  PAGES_COUNT = Math.ceil(foundBooks.length / PER_PAGE_COUNT);
+
+  if (endPage > PAGES_COUNT) {
+    startPage -= Math.abs(PAGES_COUNT - endPage);
+  }
+
+  const elPageLinksFragment = document.createDocumentFragment();
+
+  for (let pageIndex = startPage; pageIndex <= endPage; pageIndex++) {
+
+    if (pageIndex < 1) {
+      endPage++;
+      continue;
+    }
+
+    const elPaginationItem = elPagnationItemElementTemplate.cloneNode(true);
+    elPaginationItem.querySelector(".page-link").textContent = pageIndex;
+    if (pageIndex > PAGES_COUNT) {
+      break;
+    }
+
+    if (pageIndex === CURRENT_PAGE) {
+      elPaginationItem.querySelector("li").classList.add("active");
+    }
+    elPageLinksFragment.appendChild(elPaginationItem);
+  }
+
+  elPaginationList.appendChild(elPageLinksFragment);
+}
+
+
+// TRUE PAGINATION WORK CODE
+function goToPage(pageIndex) {
+  if(pageIndex > PAGES_COUNT){
+    pageIndex = PAGES_COUNT;
+  }
+
+  if(pageIndex < 1){
+    pageIndex = 1;
+  }
+
+  CURRENT_PAGE = pageIndex;
+
+  if(CURRENT_PAGE === 1){
+    elPaginationPrevLink.parentElement.classList.add("disabled");
+  }else{
+    elPaginationPrevLink.parentElement.classList.remove("disabled");
+  }
+  if(CURRENT_PAGE === PAGES_COUNT){
+    elPaginationNextLink.parentElement.classList.add("disabled");
+  }else{
+    elPaginationNextLink.parentElement.classList.remove("disabled");
+  }
+
+  showPagenation();
+}
+
+function goToPrevPage() {
+  goToPage(CURRENT_PAGE - 1);
+  showPagenation();
+
+}
+
+function goToNextPage() {
+  goToPage(CURRENT_PAGE + 1);
+  showPagenation();
+}
+
+function goToLastPage() {
+  goToPage(PAGES_COUNT);
+  showPagenation();
+}
+
+function goToFirstPage() {
+  goToPage(1);
+  showPagenation();
+}
+
 
 // ADDEVENT LISTENERS
 
@@ -249,9 +369,34 @@ if(elBooks){
   elBooks.addEventListener("click", onClickBooksList);
 }
 
+// PAGINATION
+if(elPaginationStartLink){
+  elPaginationStartLink.addEventListener("click", goToFirstPage);
+}
+if(elPaginationPrevLink){
+  elPaginationPrevLink.addEventListener("click", goToPrevPage);
+}
+
+if(elPaginationNextLink){
+  elPaginationNextLink.addEventListener("click", goToNextPage);
+}
+
+if(elPaginationEndLink){
+  elPaginationEndLink.addEventListener("click", goToLastPage);
+}
+
+if(elPaginationList){
+  elPaginationList.addEventListener("click", evt => {
+    if(evt.target.matches(".page-link")){
+      goToPage(Number(evt.target.textContent));
+    }
+  });
+}
+
 // FUNCTIONS RUN
 gettingCountries();
 showCountries();
 gettingLanguages();
 showLanguages();
-showBooksElement(books, titleRegex);
+showBooksElement(foundBooks, titleRegex);
+showPagenation();
